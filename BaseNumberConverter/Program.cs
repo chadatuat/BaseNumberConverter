9e1b59478a403450dde273cec53d6814ae879e32
Chad Weirick <cweirick@truepointsolutions.com>
2021-01-17 15:07:16 -0700

    added main comments

--- BaseNumberConverter/Program.cs
+++ BaseNumberConverter/Program.cs
@@ -1,7 +1,10 @@
 ﻿// Chad Weirick
 // UAT MS507
 // This is the main program module.
-
+// This program presents a UI, allows a user to enter a value and an input and output base and convert them.  The result will be shown in its own message box at the end of the conversion.
+// note: this program can be modified, specifically the limit of what constitutes a valid character for conversion.  These can be extended or modified to the users needs.  At present the system supports up to base 36.
+// v1.01
+// changes: added security to prevent an endless loop when converting a blank line of text.
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +20,7 @@ namespace BaseNumberConverter
         /// 
         /// Base entry point
         /// 
-        // These are the acceptable characters.  Modifying these dynamically changes the program, but you will need to manually adjust the array limit on line 22.
+        // These are the acceptable characters.  Modifying these dynamically changes the program, but you will need to manually adjust the array limit on the line after next.
         public static char[] cArrayChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
         public static int[] iArrayCount = new int[36];
         
